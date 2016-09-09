ead577e38fd49ead54e9f8eab6b339b0f31d442d
nate <nate.scott.fisher@gmail.com>
2016-09-09 14:52:00 -0700

    reduce USB library resource allocation
    - it seems there is only one endpoint, once config and one interface required

--- source/application/usb/usbh_conf.h
+++ source/application/usb/usbh_conf.h
@@ -36,14 +36,14 @@
 #include <string.h>
 
 /* Exported types ------------------------------------------------------------*/
-#define USBH_MAX_NUM_ENDPOINTS                2
-#define USBH_MAX_NUM_INTERFACES               2
-#define USBH_MAX_NUM_CONFIGURATION            2
+#define USBH_MAX_NUM_ENDPOINTS                1
+#define USBH_MAX_NUM_INTERFACES               1
+#define USBH_MAX_NUM_CONFIGURATION            1
 #define USBH_MAX_NUM_SUPPORTED_CLASS          1
 #define USBH_KEEP_CFG_DESCRIPTOR              0
 #define USBH_MAX_SIZE_CONFIGURATION           0x200
 #define USBH_MAX_DATA_BUFFER                  0x200
-#define USBH_DEBUG_LEVEL                      3
+#define USBH_DEBUG_LEVEL                      3 // TODO: disable once working
 #define USBH_USE_OS                           0
 
 /* Exported constants --------------------------------------------------------*/
