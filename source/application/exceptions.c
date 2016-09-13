0c084d7480e6f355c7ae7109752b6f16443c4595
nate <nate.scott.fisher@gmail.com>
2016-09-13 07:59:20 -0700

    add audio player reference files from ST

--- source/application/exceptions.c
+++ source/application/exceptions.c
@@ -1,7 +1,8 @@
-#include "main.h"
 #include "exceptions.h"
 
 extern HCD_HandleTypeDef hhcd;
+extern SAI_HandleTypeDef haudio_out_sai;
+extern SAI_HandleTypeDef haudio_in_sai;
 
 // Cortex-M7 Processor Exceptions Handlers
 
@@ -41,8 +42,16 @@ void SysTick_Handler(void) {
     HAL_IncTick();
 }
 
-// STM32F7xx Peripherals Interrupt Handlers 
+// STM32F7xx Peripherals Interrupt Handlers
 
 void OTG_HS_IRQHandler(void) {
     HAL_HCD_IRQHandler(&hhcd);
+}
+
+void AUDIO_OUT_SAIx_DMAx_IRQHandler(void) {
+    HAL_DMA_IRQHandler(haudio_out_sai.hdmatx);
+}
+
+void AUDIO_IN_SAIx_DMAx_IRQHandler(void) {
+    HAL_DMA_IRQHandler(haudio_in_sai.hdmarx);
 }
